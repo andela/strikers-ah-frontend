570a830f21562dd79f7f8df46765880da45b81f5
mwibutsa <mflohost@gmail.com>
2019-05-15 19:39:45 +0200

    Chore #164489737 Setup React Redux

---
+++ src/actions/userAction.js
@@ -0,0 +1,9 @@
+import { EDIT_USER_PROFILE, GET_USER_PROFILE } from './types';
+
+export const getLoggedInUserProfile = () => {
+  return (dispatch) => {};
+};
+
+export const editLoggedInUserProfile = () => {
+  return (dispatch) => {};
+};
