5632e226aa0069b771cf136aad0e05b3a2620d04
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-05-16 13:18:33 +0200

    ch configure react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/login" render={() => <h1>login</h1>} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
