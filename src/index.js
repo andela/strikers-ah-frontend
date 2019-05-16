30e2f7a83b1a8831be99776835f4fe87ba9573eb
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-05-16 15:18:16 +0200

    refactoring the routes configuration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route path="/login" render={() => <h1>login</h1>} />
-    </div>
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
