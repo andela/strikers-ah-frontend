9c9cedcfb74401addbc35d01242dd96b0a6f2b41
Frank Harerimana <frankharerimana@Franks-MacBook-Pro.local>
2019-05-19 13:26:36 +0200

    ft-164489746 testingthe login component

---
+++ src/tests/components/Login.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import '../../enzymeConfig';
+import Login from '../../components/Login';
+import SocialButtons from '../../components/SocialButtons';
+
+describe('`login.jsx`', () => {
+  test('should render', () => {
+    const wrapper = mount(<Login />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should render', () => {
+    const wrapper = mount(<SocialButtons />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
---
+++ src/tests/redux/actions/userAction.test.js
@@ -0,0 +1,11 @@
+import { getLoggedInUserProfile } from '../../../redux/actions/userAction';
+
+describe('`action.jsx`', () => {
+  test('should decode the token', () => {
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RuYW1lIjoiaGFyZXJpbWFuYSIsImxhc3RuYW1lIjoiZnJhbmsiLCJ1c2VybmFtZSI6ImZyYW5rLWhhcmVyaW1hbmE1NzgwIiwiZW1haWwiOiJoYXJmcmFuazJAZ21haWwuY29tIiwiaW1hZ2UiOiJodHRwczovL2xoNi5nb29nbGV1c2VyY29udGVudC5jb20vLW5kRGZtZjBJallrL0FBQUFBQUFBQUFJL0FBQUFBQUFBQXQ0L2ZkTEJmOFlfcTlFL3Bob3RvLmpwZyIsImlhdCI6MTU1ODIwNDAzNiwiZXhwIjoxNTU4MjkwNDM2fQ.a3A3rffzCEjToB7tRvWZ8TXI16vVKGG8SL67CRTv-GQ';
+    const res = getLoggedInUserProfile(token);
+    expect(res.type);
+    expect(res.payload);
+  });
+});
