4032ff3ad6b563b34a63c0b6b7a3f2e8d2132e0b
Inclet <mwunguzicl@gmail.com>
2019-05-28 14:04:16 +0200

    update url link

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -18,7 +18,7 @@ class SocialLogin extends PureComponent {
    */
   handlerClick(party) {
     this.action = party;
-    return `http://localhost:5000/api/auth/${this.action}`;
+    return `https://strikers-ah-backend.herokuapp.com/api/auth/${this.action}`;
   }
 
   /**
