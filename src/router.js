1ee90e6f41dc0299fb7d3ea6e98b80a0a577931f
mwibutsa <mflohost@gmail.com>
2019-06-13 19:38:23 +0200

    Feature add Navigation bar to the profilepage

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Index from './components/Index';
 import Signup from './components/signup';
-import {getLoggedInUser} from './helpers/authentication';
-import {CreateArticle} from './components/article/CreateArticle';
+import { getLoggedInUser } from './helpers/authentication';
+import { CreateArticle } from './components/article/CreateArticle';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import Profile from './components/profile';
@@ -16,6 +16,7 @@ import OneArticle from './components/article/EditArticle';
 import PrivateRoute from './PrivateRoute';
 import Settings from './components/Setting';
 import Logout from './components/logout';
+import HomeNavBar from './components/homeNavBar';
 
 const routes = () => (
   <Switch>
@@ -38,12 +39,22 @@ const routes = () => (
       test-data="profileRouter"
       path="/:username"
       render={props => {
-        const {username} = props.match.params;
+        const { username } = props.match.params;
         const loggedInUser = getLoggedInUser();
         if (username === loggedInUser.username) {
-          return <Settings username={username} />;
+          return (
+            <div>
+              <HomeNavBar user={username} />
+              <Settings username={username} />;
+            </div>
+          );
         }
-        return <Profile username={username} />;
+        return (
+          <div>
+            <HomeNavBar user="" />
+            <Profile username={username} />;
+          </div>
+        );
       }}
     />
   </Switch>
