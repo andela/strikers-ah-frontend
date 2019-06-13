9c70367b1853326a1beb34dc936f11516b531caf
mwibutsa <mflohost@gmail.com>
2019-06-13 19:02:51 +0200

    Add User menu to the navbar

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Login from './components/Login';
 import Index from './components/Index';
 import Signup from './components/signup';
-import { getLoggedInUser } from './helpers/authentication';
-import { CreateArticle } from './components/article/CreateArticle';
+import {getLoggedInUser} from './helpers/authentication';
+import {CreateArticle} from './components/article/CreateArticle';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import Profile from './components/profile';
@@ -15,6 +15,7 @@ import getOneArticle from './components/article/ReadArticle';
 import OneArticle from './components/article/EditArticle';
 import PrivateRoute from './PrivateRoute';
 import Settings from './components/Setting';
+import Logout from './components/logout';
 
 const routes = () => (
   <Switch>
@@ -24,6 +25,8 @@ const routes = () => (
     <Route path="/forgotpassword" component={ForgotPassword} />
     <Route path="/resetpassword" component={ResetPassword} />
     <Route path="/signup" component={Signup} />
+    <Route path="/profile/:username" component={Profile} />
+    <Route path="/logout" component={Logout} />
     <Route path="/" exact component={Index} />
     <PrivateRoute exact path="/article/:slug" component={getOneArticle} />
     <PrivateRoute
@@ -35,7 +38,7 @@ const routes = () => (
       test-data="profileRouter"
       path="/:username"
       render={props => {
-        const { username } = props.match.params;
+        const {username} = props.match.params;
         const loggedInUser = getLoggedInUser();
         if (username === loggedInUser.username) {
           return <Settings username={username} />;
