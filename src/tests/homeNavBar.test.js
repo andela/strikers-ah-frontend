8507fe86de55d3e275ec5808964836a92f11f6e0
mwibutsa <mflohost@gmail.com>
2019-06-14 10:41:04 +0200

    test home navbar

--- src/tests/homeNavBar.test.js
+++ src/tests/homeNavBar.test.js
@@ -10,6 +10,10 @@ const props = {
   notification: jest.fn(),
 };
 describe('<HomeNavBar/>', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<HomeNavBar {...props} />);
+  });
   it('Should render navbar with user logged in', () => {
     const navBar = shallow(<HomeNavBar {...props} />);
     expect(navBar).toMatchSnapshot();
@@ -18,5 +22,22 @@ describe('<HomeNavBar/>', () => {
   it('Should render navbar without user logged in', () => {
     const navBar = shallow(<HomeNavBar {...props} />);
     expect(navBar).toMatchSnapshot();
+  });
+  it('should openToggle', () => {
+    const instance = wrapper.instance();
+    instance.openToggle({ target: { name: 'notification' } });
+    expect(instance.state.popup).toBe(true);
+    instance.openToggle({ target: { name: 'profile' } });
+    expect(instance.state.profile).toBe(true);
+  });
+
+  it('should be able to update notifications', () => {
+    const instance = wrapper.instance();
+    instance.componentWillReceiveProps({
+      NotificationReducer: [{ message: 'message' }],
+    });
+    instance.componentWillReceiveProps({ NotificationReducer: false });
+    instance.componentWillReceiveProps({ NotificationReducer: [] });
+    expect(instance.state.notificationMsg).toBe('No notification yet');
   });
 });
