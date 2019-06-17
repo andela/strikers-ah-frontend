0bff844f85e40a1411015437d58f6679fdeb3b2f
mwibutsa <mflohost@gmail.com>
2019-06-17 11:54:55 +0200

    Feature #164489758 Create search engine

---
+++ src/redux/reducers/searchReducer.js
@@ -0,0 +1,21 @@
+import { SEARCH_RESULTS, NO_SEARCH_RESULT } from '../actionTypes/searchTypes';
+
+const initialState = {};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SEARCH_RESULTS:
+      return {
+        ...state,
+        searchResults: payload,
+      };
+    case NO_SEARCH_RESULT:
+      return {
+        ...state,
+        searchError: payload,
+      };
+
+    default:
+      return state;
+  }
+};
