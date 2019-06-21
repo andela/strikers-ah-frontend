4b21b2c262c506f574129b6fef8242338ff1e708
Inclet <mwunguzicl@gmail.com>
2019-06-21 15:24:59 +0200

    add click outside popup

--- src/components/homeNavBar.jsx
+++ src/components/homeNavBar.jsx
@@ -158,115 +158,109 @@ export class HomeNavBar extends Component {
           </Link>
           {username ? (
             <div className="user-sm-dashboard">
-              <OutsideClickHandler
-                onOutsideClick={() => {
-                  popup && this.setState({ popup: false });
-                }}
-              >
-                <img
-                  className="icons-style user-profile"
-                  src={userProfile}
-                  alt="profile"
-                  onClick={this.openToggle.bind(this)}
-                  name="profile"
-                  role="button"
-                />
-              </OutsideClickHandler>
-              <OutsideClickHandler
-                onOutsideClick={() => {
-                  popup && this.setState({ popup: false });
-                }}
-              >
-                <img
-                  className="icons-style notification-icon"
-                  src={notifications}
-                  alt="notification"
-                  onClick={this.openToggle.bind(this)}
-                  role="button"
-                  name="notification"
-                />
-              </OutsideClickHandler>
+              <img
+                className="icons-style user-profile"
+                src={userProfile}
+                alt="profile"
+                onClick={this.openToggle.bind(this)}
+                name="profile"
+                role="button"
+              />
+              <img
+                className="icons-style notification-icon"
+                src={notifications}
+                alt="notification"
+                onClick={this.openToggle.bind(this)}
+                role="button"
+                name="notification"
+              />
               <img className="icons-style" src={search} alt="search" />
               {isNotified && <div className="notified"> </div>}
               {popup && (
-                <div className="popup-style">
-                  {userNotification && (
-                    <React.Fragment>
-                      <div className="arrow-up-notification-back" />
-                      <div className="arrow-up-notification" />
-                      <div className="notification-style">
-                        {allNotifications.length === 0
-                          ? notificationMsg
-                          : _.take(allNotifications, 5).map(element => (
-                              <React.Fragment key={element.id}>
-                                <div
-                                  className={
-                                    element.status === 'Read'
-                                      ? 'read_notification'
-                                      : 'notifications'
-                                  }
-                                  onClick={() =>
-                                    this.handleClickNotification(element.id)
-                                  }
-                                  role="button"
-                                >
-                                  <img
-                                    className="notification-icon"
-                                    src={notificationIcon}
-                                    alt="new notification"
-                                  />
-                                  <Link
-                                    className="notification-article-link"
-                                    to={_.takeRight(
-                                      element.link.split('/'),
-                                      2,
-                                    ).join('/')}
+                <OutsideClickHandler
+                  onOutsideClick={() => {
+                    popup && this.setState({ popup: false });
+                  }}
+                >
+                  <div className="popup-style">
+                    {userNotification && (
+                      <React.Fragment>
+                        <div className="arrow-up-notification-back" />
+                        <div className="arrow-up-notification" />
+                        <div className="notification-style">
+                          {allNotifications.length === 0
+                            ? notificationMsg
+                            : _.take(allNotifications, 5).map(element => (
+                                <React.Fragment key={element.id}>
+                                  <div
+                                    className={
+                                      element.status === 'Read'
+                                        ? 'read_notification'
+                                        : 'notifications'
+                                    }
+                                    onClick={() =>
+                                      this.handleClickNotification(element.id)
+                                    }
+                                    role="button"
                                   >
-                                    <span>{element.message}</span>
-                                    <div className="notification-time">
-                                      <Moment fromNow>
-                                        {element.createdAt}
-                                      </Moment>
-                                    </div>
-                                  </Link>
-                                </div>
-                                <hr className="notification-horizontal-line" />
-                              </React.Fragment>
-                            ))}
-                        {allNotifications.length === 0 ? (
-                          ' '
-                        ) : (
-                          <div className="seeAll-style">
-                            <Link to="/notifications" className="seeAll-link">
-                              SEE ALL
-                            </Link>
-                          </div>
-                        )}
-                      </div>
-                    </React.Fragment>
-                  )}
-                  {profile && (
-                    <React.Fragment>
-                      <div className="arrow-up-profile-back" />
-                      <div className="arrow-up-profile" />
-                      <Link
-                        className="profile-style user-menu"
-                        to={`/${username}`}
-                      >
-                        Profile
-                      </Link>
-                      <Link
-                        className="profile-style user-menu"
-                        to="/article/create"
-                      >
-                        Create Article
-                      </Link>
-                      <Link className="profile-style user-menu" to="/logout">
-                        Logout
-                      </Link>
-                    </React.Fragment>
-                  )}
-                </div>
+                                    <img
+                                      className="notification-icon"
+                                      src={notificationIcon}
+                                      alt="new notification"
+                                    />
+                                    <Link
+                                      className="notification-article-link"
+                                      to={_.takeRight(
+                                        element.link.split('/'),
+                                        2,
+                                      ).join('/')}
+                                    >
+                                      <span>{element.message}</span>
+                                      <div className="notification-time">
+                                        <Moment fromNow>
+                                          {element.createdAt}
+                                        </Moment>
+                                      </div>
+                                    </Link>
+                                  </div>
+                                  <hr className="notification-horizontal-line" />
+                                </React.Fragment>
+                              ))}
+                          {allNotifications.length === 0 ? (
+                            ' '
+                          ) : (
+                            <div className="seeAll-style">
+                              <Link to="/notifications" className="seeAll-link">
+                                SEE ALL
+                              </Link>
+                            </div>
+                          )}
+                        </div>
+                      </React.Fragment>
+                    )}
+                    {profile && (
+                      <React.Fragment>
+                        <div className="arrow-up-profile-back" />
+                        <div className="arrow-up-profile" />
+                        <Link
+                          className="profile-style user-menu"
+                          to={`/${username}`}
+                        >
+                          Profile
+                        </Link>
+                        <Link
+                          className="profile-style user-menu"
+                          to="/article/create"
+                        >
+                          Create Article
+                        </Link>
+                        <Link className="profile-style user-menu" to="/logout">
+                          Logout
+                        </Link>
+                      </React.Fragment>
+                    )}
+                  </div>
+                </OutsideClickHandler>
               )}
             </div>
           ) : (
